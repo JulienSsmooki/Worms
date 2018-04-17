e7c76c9a1a1e853b54430b763aca54cc0d24ae38
Unknown <julien.lopez.30100@gmail.com>
2018-04-18 01:10:49 +0200

    GameLoop
    Gestion de la game loop tour par tour / selection du worms appartenant au joueur / controle du worms en question => next player

--- WormsLike/Assets/Worms/Scripts/Game/PlayerController.cs
+++ WormsLike/Assets/Worms/Scripts/Game/PlayerController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     #region Public Variables
-    
+
+    public bool isControledWorms = false;
+
+    [Range(0.0f, 2.0f)] public float scaleDeplacement = 1.0f;
+
+    public float lifePoint = 100.0f;
 
     #endregion
 
@@ -14,6 +19,12 @@ public class PlayerController : MonoBehaviour {
     #region Private Variables
 
     NetworkManager NetManager;
+
+    GameManager GM;
+
+    Rigidbody2D rb2D;
+    Vector2 velocity = Vector2.zero;
+    bool isGrounded = false;
 
     #endregion
 
@@ -24,14 +35,49 @@ public class PlayerController : MonoBehaviour {
     private void Awake()
     {
         NetManager = FindObjectOfType<NetworkManager>();
-        
+        GM = FindObjectOfType<GameManager>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(isControledWorms)
+        {
+            Vector2 moveX = Vector2.zero;
+            moveX.x = Input.GetAxis("Horizontal") * scaleDeplacement * Time.deltaTime + rb2D.position.x;
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = 1.0f;
+            }
+            if(!isGrounded)
+            {
+                velocity.y -= rb2D.gravityScale * Time.deltaTime;
+            }
+
+            rb2D.velocity = velocity;
+            moveX.y = rb2D.position.y;
+            rb2D.position = moveX;
+        }
+    }
+
+    
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        isGrounded = false;
     }
 
     #endregion
 
 
     #region Photon.PunBehaviour CallBacks
-    
+
 
     #endregion
 }
--- WormsLike/Assets/Worms/Scripts/Manager/GameManager.cs
+++ WormsLike/Assets/Worms/Scripts/Manager/GameManager.cs
@@ -2,113 +2,219 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GamePhase
+{
+    Debut,
+    SelectionWorm,
+    Action,
+    Fin
+}
+
+
 public class GameManager : MonoBehaviour {
 
     #region Public Variables
 
-    public List<GameObject> P1Worms = new List<GameObject>();
-    public List<GameObject> P2Worms = new List<GameObject>();
-    public List<GameObject> P3Worms = new List<GameObject>();
-    public List<GameObject> P4Worms = new List<GameObject>();
+    public new Camera camera;
+
+    public List<GameObject> localWorms = new List<GameObject>();
+
+    public float timerSelection;
+    public float timerAction;
+
+    public GamePhase phase = GamePhase.Debut;
+    public int idPlayerToPlay = 0;
+    public PhotonPlayer playerTurn;
+
+    public GameObject selectedWorm;
+    public int indexSelectWorms = 0;
 
     #endregion
-
-
+    
 
     #region Private Variables
+
+    float maxTimerSelect = 5.0f;
+    float maxTimerAction = 5.0f;
 
     NetworkManager NetManager;
 
     PhotonView view;
+    PhotonView viewCamera;
 
+    PlayerController controlPlayer;
+
+
+    bool isListClean = false;
     #endregion
-
-
+    
 
     #region MonoBehaviour CallBacks
 
     private void Awake()
     {
+        camera = Camera.main;
+        viewCamera = camera.GetComponent<PhotonView>();
+        view = GetComponent<PhotonView>();
         NetManager = FindObjectOfType<NetworkManager>();
 
         if (PhotonNetwork.isMasterClient)
         {
             GameObject[] spawns = GameObject.FindGameObjectsWithTag("Spawn");
-            NetManager.hashSet = PhotonNetwork.room.CustomProperties;
-
-            for (int i = 0; i < (int)NetManager.ToG; i++)
+            List<Vector3> spawnsPos = new List<Vector3>();
+            foreach (GameObject spawn in spawns)
             {
-                for (int j = 0; j < (int)NetManager.hashSet["NbrWorms"]; j++)
-                {
-                    GameObject tmp = PhotonNetwork.Instantiate("PlayerPrefab",new Vector2(
-                        Random.Range( spawns[(i * (int)NetManager.ToG)].transform.position.x, spawns[(i * (int)NetManager.ToG) + 1].transform.position.x),
-                        Random.Range( spawns[(i * (int)NetManager.ToG)].transform.position.y, spawns[(i * (int)NetManager.ToG) + 1].transform.position.y))
-                        , Quaternion.identity, 0);
-                    tmp.name = tmp.name + i;
-                    tmp.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.playerList[i]);
-                    switch (i)
-                    {
-                        case 0:
-                            P1Worms.Add(tmp);
-                            break;
-                        case 1:
-                            P2Worms.Add(tmp);
-                            break;
-                        case 2:
-                            P3Worms.Add(tmp);
-                            break;
-                        case 3:
-                            P4Worms.Add(tmp);
-                            break;
-                        default: break;
-                    }
-                }
+                spawnsPos.Add(spawn.transform.position);
             }
+
+
+            NetManager.hashSet = PhotonNetwork.room.CustomProperties;
+            
+            view.RPC("UpdateLocalWormsList", PhotonTargets.AllBuffered, spawnsPos.ToArray());
+
         }
     }
     
     private void Start()
     {
-        if(!PhotonNetwork.isMasterClient)
+        if(PhotonNetwork.isMasterClient)
         {
-            StartCoroutine(FindWorms());
+            playerTurn = PhotonNetwork.playerList[0];
+        }
+    }
+    
+    private void Update()
+    {
+
+        if (selectedWorm != null && playerTurn != null)
+        {
+            if (playerTurn != viewCamera.owner)
+            {
+                viewCamera.TransferOwnership(playerTurn.ID);
+            }
+            camera.transform.position = new Vector3(selectedWorm.transform.position.x, selectedWorm.transform.position.y, camera.transform.position.z);
+        }
+        if (PhotonNetwork.isMasterClient)
+        {
+            view.RPC("UpdatePlayerTurn", PhotonTargets.AllBuffered, playerTurn);
+        }
+
+        switch (phase)
+        {
+            case GamePhase.Debut:
+                timerSelection = maxTimerSelect;
+                timerAction = maxTimerAction;
+                selectedWorm = null;
+                
+                phase = GamePhase.SelectionWorm;
+                break;
+            case GamePhase.SelectionWorm:
+                if (timerSelection >= 0.0f)
+                {
+                    timerSelection -= Time.deltaTime;
+                    if (PhotonNetwork.player == playerTurn && isListClean)
+                    {
+                        int maxWorms = 0;
+                        
+                        selectedWorm = localWorms[indexSelectWorms];
+                        maxWorms = localWorms.Count;
+                        
+                        if(Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.UpArrow) && indexSelectWorms < maxWorms)
+                        {
+                            indexSelectWorms = ++indexSelectWorms % maxWorms;
+
+                        }
+                        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && indexSelectWorms > 0)
+                        {
+                            indexSelectWorms = --indexSelectWorms % maxWorms;
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    phase = GamePhase.Action;
+                }
+
+                break;
+            case GamePhase.Action:
+                if (timerAction >= 0.0f)
+                {
+                    timerAction -= Time.deltaTime;
+                    if (PhotonNetwork.player == playerTurn && isListClean)
+                    {
+                        if (controlPlayer == null && selectedWorm != null)
+                        {
+                            controlPlayer = selectedWorm.GetComponent<PlayerController>();
+                            controlPlayer.isControledWorms = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (controlPlayer != null)
+                    {
+                        controlPlayer.isControledWorms = false;
+                        controlPlayer = null;
+                    }
+                    phase = GamePhase.Fin;
+                }
+                break;
+            case GamePhase.Fin:
+                indexSelectWorms = 0;
+                idPlayerToPlay = ++idPlayerToPlay % (int)NetManager.ToG;
+
+                view.RPC("NextPlayerTurn", PhotonTargets.MasterClient, idPlayerToPlay);
+                
+                view.RPC("SecureEndTurn", PhotonTargets.AllBuffered, null);
+
+                break;
         }
     }
 
-    public IEnumerator FindWorms()
-    {
-        while(true)
-        {
-            GameObject[] worms = GameObject.FindGameObjectsWithTag("Player");
-            if(worms != null && worms.Length > 0)
-            {
-                foreach (GameObject worm in worms)
-                {
-                    switch (worm.GetComponent<PhotonView>().ownerId - 1)
-                    {
-                        case 0:
-                            P1Worms.Add(worm);
-                            break;
-                        case 1:
-                            P2Worms.Add(worm);
-                            break;
-                        case 2:
-                            P3Worms.Add(worm);
-                            break;
-                        case 3:
-                            P4Worms.Add(worm);
-                            break;
-                        default: break;
-                    }
-                }
-                Debug.Log("Find !");
-                yield break;
-            }
-            Debug.Log("Don't find !");
-            yield return 0;
-        }
-    }
 
     #endregion
 
+
+    #region Photon.PunBehaviour RPCs
+
+    [PunRPC]
+    public void SecureEndTurn()
+    {
+        phase = GamePhase.Debut;
+    }
+
+
+    [PunRPC]
+    public void UpdatePlayerTurn(PhotonPlayer _playerTurn)
+    {
+        playerTurn = _playerTurn;
+    }
+
+    [PunRPC]
+    public void NextPlayerTurn(int _idPlayerToPlay)
+    {
+        playerTurn = PhotonNetwork.playerList[_idPlayerToPlay];
+    }
+
+    [PunRPC]
+    public void UpdateLocalWormsList(Vector3[] _spawns)
+    {
+        for (int j = 0; j < (int)NetManager.hashSet["NbrWorms"]; j++)
+        {
+            GameObject tmp = PhotonNetwork.Instantiate("PlayerPrefab", new Vector2(
+                Random.Range(_spawns[((PhotonNetwork.player.ID -1) * (int)NetManager.ToG)].x, _spawns[((PhotonNetwork.player.ID -1) * (int)NetManager.ToG) + 1].x),
+                Random.Range(_spawns[((PhotonNetwork.player.ID -1) * (int)NetManager.ToG)].y, _spawns[((PhotonNetwork.player.ID -1) * (int)NetManager.ToG) + 1].y))
+                , Quaternion.identity, 0);
+            tmp.name = tmp.name + (PhotonNetwork.player.ID - 1);
+
+            localWorms.Add(tmp);
+
+        }
+
+        isListClean = true;
+    }
+
+
+    #endregion
 }
